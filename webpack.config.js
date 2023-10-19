9a00df14a15f1468f2572b90fd5a4fcd6de36ade
Shevchenko Andrey <demorest49de@gmail.com>
2023-10-19 11:36:23 +0300

    Changes package.json webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 module.exports = {
+    devtool: 'source-map',
+    devServer: {
+        hot: true,
+    },
     entry: './src/index.js',
     output: {
         filename: "index.js"
     },
+    
 }
