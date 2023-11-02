607596e62a36f1d8aeace1f4ad04814089c14fd4
Shevchenko Andrey <demorest49de@gmail.com>
2023-11-02 18:38:42 +0300

    Changes Timer.js changed; Tomato.js changed;

--- src/js/Timer.js
+++ src/js/Timer.js
@@ -2,32 +2,32 @@ export class Timer {
   #name;
   #counter;
   #remainingTime;
-  
+
   constructor(name = 'томат',
-              counter = 0,
-              remainingTime = NaN,
+      counter = 0,
+      remainingTime = NaN,
   ) {
     this.#name = name;
     this.#counter = counter;
     this.#remainingTime = remainingTime;
   }
-  
+
   increaseCounter() {
     this.#counter += 1;
   }
-  
+
   changeName(value) {
     this.#name = value;
   }
-  
+
   get name() {
     return this.#name;
   }
-  
+
   get counter() {
     return this.#counter;
   }
-  
+
   startTimer() {
     const remainingTime = this.#remainingTime;
     let sec = remainingTime;
--- src/js/Tomato.js
+++ src/js/Tomato.js
@@ -6,28 +6,28 @@ export class Tomato {
   #bigPauseTime;
   #tasks;
   #activeTask;
-  
+
   // test
   #secondsMin = 1;
-  
+
   constructor({
-                estimatedTime = 6 * this.#secondsMin,
-                pauseTime = 6 * this.#secondsMin,
-                bigPauseTime = 6 * this.#secondsMin,
-                tasks = [],
-              }) {
+    estimatedTime = 6 * this.#secondsMin,
+    pauseTime = 6 * this.#secondsMin,
+    bigPauseTime = 6 * this.#secondsMin,
+    tasks = [],
+  }) {
     this.#estimatedTime = estimatedTime;
     this.#pauseTime = pauseTime;
     this.#bigPauseTime = bigPauseTime;
     this.#tasks = tasks;
     this.#activeTask = null;
-    
+
     this.init();
-    
+
     // test
     this.proceedTask();
   }
-  
+
   init() {
     // test
     if (!localStorage.getItem('pomidor')) {
@@ -41,28 +41,28 @@ export class Tomato {
       });
       localStorage.setItem('pomidor', pomidor);
     }
-    
+
     this.#activeTask = JSON.parse(localStorage.getItem('pomidor'));
-    
+
     // this.#activeTask = this.activeTask;
   }
-  
+
   get time() {
     return {estimatedTime: this.#estimatedTime, pauseTime: this.#pauseTime, bigPauseTime: this.#bigPauseTime};
   }
-  
+
   get tasks() {
     return this.#tasks;
   }
-  
+
   get activeTask() {
     return this.#tasks.find(t => t.isActive);
   }
-  
+
   addTask(task) {
     this.#tasks.push(task);
   }
-  
+
   activateTask(id) {
     this.deactivateAllTasks();
     for (const task of this.#tasks) {
@@ -72,7 +72,7 @@ export class Tomato {
       }
     }
   }
-  
+
   increaseCounter(id) {
     // test
     this.#activeTask.counter += 1;
@@ -83,13 +83,13 @@ export class Tomato {
     //     }
     // }
   }
-  
+
   deactivateAllTasks() {
     for (const task of this.#tasks) {
       if (task.isActive) task.isActive = false;
     }
   }
-  
+
   pomidorType(remainingTime, counter) {
     const {estimatedTime, pauseTime, bigPauseTime} = this.time;
     switch (true) {
@@ -104,24 +104,24 @@ export class Tomato {
         return remainingTime = remainingTime ? remainingTime : estimatedTime;
     }
   }
-  
+
   proceedTask() {
     if (this.#activeTask) {
       const task = this.#activeTask;
       let remainingTime = task.remainingTime;
-      
+
       this.increaseCounter(task.id);
       console.log(' this.#activeTask.counter: ', this.#activeTask.counter);
-      
+
       remainingTime = this.pomidorType(remainingTime, task.counter);
-      
+
       const timer = new Timer(task.name, task.counter, remainingTime);
       console.log(' this.#activeTask: ', this.#activeTask);
       const timerPromise = timer.startTimer();
       timerPromise.then(() => {
         this.#activeTask.remainingTime = 0;
         console.log(' this.#activeTask: ', this.#activeTask);
-        
+
         // test
         localStorage.setItem('pomidor', JSON.stringify(this.#activeTask));
         window.location.href = `http://localhost:63342/tomato-timer/src/index.html`;
